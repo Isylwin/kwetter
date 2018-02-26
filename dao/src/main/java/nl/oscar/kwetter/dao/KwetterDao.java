f7a10b387d17d3ba006551dca19c1a3f809d20e0
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2018-02-26 15:44:20 +0100

    Added KwetterDaoCollection

---
+++ dao/src/main/java/nl/oscar/kwetter/dao/KwetterDao.java
@@ -0,0 +1,6 @@
+package nl.oscar.kwetter.dao;
+
+import nl.oscar.kwetter.domain.Kwetter;
+
+public interface KwetterDao extends Dao<Long, Kwetter> {
+}
---
+++ dao/src/main/java/nl/oscar/kwetter/dao/impl/KwetterDaoCollection.java
@@ -0,0 +1,42 @@
+package nl.oscar.kwetter.dao.impl;
+
+import nl.oscar.kwetter.dao.KwetterDao;
+import nl.oscar.kwetter.domain.Kwetter;
+
+import javax.ejb.Singleton;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Singleton
+public class KwetterDaoCollection implements KwetterDao {
+
+    private static long id;
+    private Map<Long, Kwetter> map = new HashMap<>();
+
+    @Override
+    public void create(Kwetter object) {
+        object.setId(id++);
+        map.put(object.getId(), object);
+    }
+
+    @Override
+    public void edit(Kwetter object) {
+        map.put(object.getId(), object);
+    }
+
+    @Override
+    public Kwetter find(Long id) {
+        return map.get(id);
+    }
+
+    @Override
+    public Collection<Kwetter> findAll() {
+        return map.values();
+    }
+
+    @Override
+    public void remove(Kwetter object) {
+        map.remove(object.getId());
+    }
+}
